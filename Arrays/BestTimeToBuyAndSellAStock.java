94b9d5621a209e1dd8eacd29beb139b096509637
Oluwatorinmo Ajanaku <oluwatorinmo.ajanaku@zedcrest.com>
2024-11-22 04:27:45 +0100

    Maximum profit that can be gotten from selling a stock

---
+++ Arrays/BestTimeToBuyAndSellAStock.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+public class BestTimeToBuyAndSellAStock {
+    //You are given an array prices where prices[1] is the price of a given stock on the ith day.
+    //You want to maximize your  profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+    //Return the maximum profit you can make from this transaction.If you cannot achieve any profit, return  0.
+
+    public static int BuyAndSellStockMaxProfit(int[] prices){
+        int minPrice = Integer.MAX_VALUE;
+        int maximumProfit = 0;
+         for(int index = 0; index < prices.length; index++){
+            if(prices[index]< minPrice){
+                minPrice = prices[index];
+            }
+            else{
+                int profit = prices[index] - minPrice;
+                maximumProfit = Math.max(maximumProfit,profit);
+            } 
+         }
+         return maximumProfit;
+
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,4,7,0};
+        int maxProfit = BuyAndSellStockMaxProfit(arr);
+        System.out.println(maxProfit);
+    }
+}
